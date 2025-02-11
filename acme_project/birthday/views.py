9b378b3a86a76ed514e5e6670b9063c87d8186a8
Buggu <last.coin@yandex.ru>
2025-02-12 00:07:15 +0300

    add CBV-view classes

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,8 +1,39 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
+
+
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 3
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
 
 
 def birthday(request, pk=None):
@@ -27,6 +58,16 @@ def birthday(request, pk=None):
 def birthday_list(request):
     birthdays = Birthday.objects.all()
     context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
+
+
+def birthday_list(request):
+    birthdays = Birthday.objects.order_by('id')
+    paginator = Paginator(birthdays, 3)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
 
 
